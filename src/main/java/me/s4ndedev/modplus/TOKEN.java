b4803fb68c7c863a98e830e13784f0c277c64777
Sander <52680010+s4ndedev@users.noreply.github.com>
2021-01-30 01:15:16 +0100

    delete

--- src/main/java/me/s4ndedev/modplus/TOKEN.java
+++
@@ -1,8 +0,0 @@
-package me.s4ndedev.modplus;
-
-public class TOKEN {
-
-    // shhhh, this is my secret token
-    public static String TOKEN = "ODA0MTAwODQzMTc5NDc1MDA1.YBHbRA.VF6BDjHyaXoNxY3-hU-BO32TPis";
-
-}
